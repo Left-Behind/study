6492adeb90055a050009dd53fac4d0e8dd7c2d62
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-08-08 16:52:41 +0800

    添加了一个QQ互联的小demo,阿里云域名还没通过无法测试

---
+++ qq/src/main/java/com/azhu/qq/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.azhu.qq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Azhu
+ * @date 2019/8/6 11:53
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(HttpSession session, Model model){
+        String openid = (String) session.getAttribute("openid");
+        String nickname = (String) session.getAttribute("nickname");
+        String figureurl_qq_2 = (String) session.getAttribute("figureurl_qq_2");
+
+        model.addAttribute("openid",openid);
+        model.addAttribute("nickname",nickname);
+        model.addAttribute("figureurl_qq_2",figureurl_qq_2);
+
+        return "home";
+    }
+}
