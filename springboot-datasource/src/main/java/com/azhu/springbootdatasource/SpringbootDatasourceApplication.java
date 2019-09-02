1c2e48bf35c5c865529177ed16083ca3f5c97f18
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-09-02 20:28:28 +0800

    Merge branch 'master' of /home/azhu/Github/study with conflicts.

--- springboot-datasource/src/main/java/com/azhu/springbootdatasource/SpringbootDatasourceApplication.java
+++ springboot-datasource/src/main/java/com/azhu/springbootdatasource/SpringbootDatasourceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.azhu.springbootdatasource.mapper")
+@MapperScan("com.azhu.springbootdatasource.mapper")//这个和mapper层的@Data注解作用一样,可以都加,
 public class SpringbootDatasourceApplication {
 
     public static void main(String[] args) {
--- springboot-datasource/src/main/java/com/azhu/springbootdatasource/mapper/UserInfoMapper.java
+++ springboot-datasource/src/main/java/com/azhu/springbootdatasource/mapper/UserInfoMapper.java
@@ -10,21 +10,18 @@ import java.util.List;
  * @Date: 2019/5/1 17:43
  * Description:
  */
+@Mapper
 public interface UserInfoMapper {
 
-    @Insert("insert into user_ (userName,password,avatarUrl,email) values (#{userName},#{password},#{avatarUrl},#{email})")
+
     Integer insertUserInfo(UserInfo userInfo);
 
-    @Update("update user_ set userName=#{userName},password=#{password},avatarUrl=#{avatarUrl} where userId=#{userId}")
     Integer updateUserInfo(UserInfo userInfo);
 
-    @Delete("delete from user_ where userId= #{id}")
     Integer deleteUserInfoById(Integer id);
 
-    @Select("select userId,userName,avatarUrl,email from user_ where userId=#{id}")//这里不能获取密码字段 这里返回的应该是一个简单的UserInfo对象
     UserInfo queryUserInfoByUserId(Integer id);
 
-    @Select("select * from user")
     List<UserInfo> queryAllUserInfoList();
 
 
