77becfd67747c40eec94351fb59bd194f40d06ca
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-10-11 15:54:57 +0800

    :memo:添加注解

--- springboot-session-redis/src/main/java/com/azhu/springbootsessionredis/SpringbootSessionRedisApplication.java
+++ springboot-session-redis/src/main/java/com/azhu/springbootsessionredis/SpringbootSessionRedisApplication.java
@@ -8,6 +8,21 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 @EnableCaching
 @EnableRedisHttpSession  //开启springboot-session-redis 把session存放到指定的redis里
+//这里好像不用配置注解和yml配置文件里不需要 session:
+//    store-type: redis # spring session使
+//    只要pom.xml里添加这两个依赖就行，
+/**
+ *         <dependency>
+ *             <groupId>org.springframework.boot</groupId>
+ *             <artifactId>spring-boot-starter-data-redis</artifactId>
+ *         </dependency>
+ *         <!-- https://mvnrepository.com/artifact/org.springframework.session/spring-session-data-redis -->
+ *         <dependency>
+ *             <groupId>org.springframework.session</groupId>
+ *             <artifactId>spring-session-data-redis</artifactId>
+ *             <version>2.1.3.RELEASE</version>
+ *         </dependency>
+ */
 @SpringBootApplication
 public class SpringbootSessionRedisApplication {
 
