1b183556eeb575cde961264dbb2b89b3db9b04a0
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-11-11 15:51:53 +0800

    :boom:添加springboot-netty模块

---
+++ springboot-netty/src/test/java/work/azhu/springbootnetty/SpringbootNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package work.azhu.springbootnetty;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
